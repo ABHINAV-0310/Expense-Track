92c05ef237b400cf7187794b354172b9ecc6cf47
sanjanawadhwa <87617603+sanjanawadhwa@users.noreply.github.com>
2022-12-10 10:25:48 +0530

    changes done
    changes done
    
    Co-Authored-By: ABHINAV-0310 <84863014+ABHINAV-0310@users.noreply.github.com>

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,8 +1,16 @@
+const model = require('../models/model');
 
-
-//get categories
+// post:http://localhost:8080/api/categories
 function create_Categories(req,res){
-    res.json("Get Request From Categories")
+  const Create = new model.Categories({
+    type:"Savings",
+    color:'#1F3B5C',//dark
+  })
+
+  Create.save(function(err){
+    if(!err)returnres.json(Create);
+    return res.status(400).json({message:'Error while creating categories ${err}'});
+  });
 }
 
 module.exports = {
